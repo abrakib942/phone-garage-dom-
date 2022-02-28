e03a300583f1f7e26c9d9f260c4d0745db1cb31c
abrakib942 <abrakib942@gmail.com>
2022-03-01 00:14:00 +0600

    fetch

---
+++ app.js
@@ -0,0 +1,12 @@
+const searchPhone = ()  => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+
+    searchField.value = '';
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
