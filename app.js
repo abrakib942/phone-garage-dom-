7581c6be3840d4d002d523ff4ce49b8901c3cbf4
abrakib942 <abrakib942@gmail.com>
2022-03-01 11:43:24 +0600

    card added

--- app.js
+++ app.js
@@ -13,7 +13,23 @@ const searchPhone = ()  => {
 
 const displaySearch = phones => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     for(const phone of phones) {
-        console.log(phone);
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="col">
+            <div class="card m-3 rounded">
+            <img src="${phone.image}" class="card-img-top p-4" alt="...">
+            <div class="card-body">
+                <h4 class="card-title  text-primary">${phone.brand} ${phone.phone_name}</h4>
+                <button onclick= "phoneDetails()" class= "btn btn-danger">Details</button>
+            </div>
+            </div>
+        </div>`
+
+        searchResult.appendChild(div)
     }
+}
+
+const phoneDetails = () => {
+    console.log('clicked'); 
 }
